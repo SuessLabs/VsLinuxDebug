ef2a625290b8fc9aec43b1c5e49197c28f690832
Damian Suess <djsuess@gmail.com>
2022-03-25 19:01:46 -0400

    serializer

--- src/VsLinuxDebugger/Core/LaunchSettings.cs
+++ src/VsLinuxDebugger/Core/LaunchSettings.cs
@@ -6,26 +6,42 @@ namespace VsLinuxDebugger.Core
   internal class LaunchSettings
   {
     internal const string AdapterFileName = "launch.json";
-
-    private readonly string _dotNetPath;
-    private readonly string _ipAddress;
-    private readonly string _outputFolder;
-    private readonly string _userName;
-    private readonly string _userPass;
-    private readonly string _vsDbgPath;
-
-    internal LaunchSettings(string ipAddress, string userName, string password, string vsDbgPath, string dotNetPath, string outputFolder)
-    {
-      _ipAddress = ipAddress;
-      _userName = userName;
-      _userPass = password;
-      _vsDbgPath = vsDbgPath;
-      _dotNetPath = dotNetPath;
-      _outputFolder = outputFolder;
-    }
+    internal static string DotNetPath;
+    internal static string IpAddress;
+    internal static string OutputFolder;
+    internal static string UserName;
+    internal static string UserPass;
+    internal static string VsDbgPath;
 
     internal static string HomePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
+    internal LaunchSettings(string ipAddress, string userName, string password, string vsDbgPath, string dotNetPath, string outputFolder)
+    {
+      IpAddress = ipAddress;
+      UserName = userName;
+      UserPass = password;
+      VsDbgPath = vsDbgPath;
+      DotNetPath = dotNetPath;
+      OutputFolder = outputFolder;
+    }
+
     internal static string SshKeyPath => Path.Combine(HomePath, ".ssh\\id_rsa");
+
+    internal string CommandLineArgs { get; set; } = string.Empty;
+
+    internal string ToJson()
+    {
+      var launch = new Launch();
+      return System.Text.Json.JsonSerializer.Serialize(launch);
+    }
+
+    private class Launch
+    {
+      public string Adapter => "ssh.exe";
+
+      public string AdapterArgs => $"-i {SshKeyPath} {UserName}@{IpAddress} {VsDbgPath} --interpreter=vscode";
+
+      public string Version => "0.2.0";
+    }
   }
 }
