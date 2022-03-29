04cd54a2ab957aa550186db6660d0b84435ce730
Damian Suess <djsuess@gmail.com>
2022-03-29 11:53:38 -0400

    Updated Tools Options sub menu to follow VS standards, "general"

--- src/VsLinuxDebugger/DebuggerPackage.cs
+++ src/VsLinuxDebugger/DebuggerPackage.cs
@@ -7,27 +7,25 @@ using Task = System.Threading.Tasks.Task;
 
 namespace VsLinuxDebugger
 {
-  /// <summary>
-  /// This is the class that implements the package exposed by this assembly.
-  /// </summary>
+  /// <summary>This is the class that implements the package exposed by this assembly.</summary>
   /// <remarks>
-  /// <para>
-  /// The minimum requirement for a class to be considered a valid package for Visual Studio
-  /// is to implement the IVsPackage interface and register itself with the shell.
-  /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-  /// to do it: it derives from the Package class that provides the implementation of the
-  /// IVsPackage interface and uses the registration attributes defined in the framework to
-  /// register itself and its components with the shell. These attributes tell the pkgdef creation
-  /// utility what data to put into .pkgdef file.
-  /// </para>
-  /// <para>
-  /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-  /// </para>
+  ///   <para>
+  ///     The minimum requirement for a class to be considered a valid package for Visual Studio
+  ///     is to implement the IVsPackage interface and register itself with the shell.
+  ///     This package uses the helper classes defined inside the Managed Package Framework (MPF)
+  ///     to do it: it derives from the Package class that provides the implementation of the
+  ///     IVsPackage interface and uses the registration attributes defined in the framework to
+  ///     register itself and its components with the shell. These attributes tell the pkgdef creation
+  ///     utility what data to put into .pkgdef file.
+  ///   </para>
+  ///   <para>
+  ///     To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+  ///   </para>
   /// </remarks>
   [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
   [Guid(DebuggerPackage.PackageGuidString)]
   [ProvideMenuResource("Menus.ctmenu", 1)]
-  [ProvideOptionPage(typeof(OptionsPage), "Linux Debugger", "Options", 0, 0, true)]
+  [ProvideOptionPage(typeof(OptionsPage), "Linux Debugger", "General", 0, 0, true)]
   public sealed partial class DebuggerPackage : AsyncPackage
   {
     /// <summary>Package GUID string.</summary>
